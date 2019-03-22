81f2449032031499c5ca3977ceadecd326f42f9a
Dillon Hoa <mr_d_2chat@hotmail.com>
2019-03-22 10:02:19 +0000

    Initial Commit

---
+++ VideoCompress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoCompress
+//
+//  Created by Dillon Hoa on 22/03/2019.
+//  Copyright © 2019 Dillon Hoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
