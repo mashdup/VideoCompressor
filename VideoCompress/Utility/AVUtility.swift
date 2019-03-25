fad700a7389ec2f2583bbbe2e567c8e7725ac3c8
Dillon Hoa <mr_d_2chat@hotmail.com>
2019-03-25 21:58:50 +0000

    Exporting video to 320x320 Square crop
    Using built in player to play exported video

---
+++ VideoCompress/Utility/AVUtility.swift
@@ -0,0 +1,85 @@
+//
+//  AVUtility.swift
+//  VideoCompress
+//
+//  Created by Dillon Hoa on 22/03/2019.
+//  Copyright © 2019 Dillon Hoa. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+import AVKit
+
+class AVUtility {
+    
+    static let fixedDimensions = 320
+    static let fixedFramesPerSecond = 30
+    /*
+     * compressVideo static method
+     * inputURL - URL of video
+     * outputURL - export URL destination
+     * handler - export session created
+     * completion - upon completion of export with success (or not)
+     */
+    
+    static func compressVideo(inputURL: URL,
+                              outputURL: URL,
+                              session:@escaping (_ exportSession: AVAssetExportSession?)-> Void,
+                              completion:@escaping(_ exportSession: AVAssetExportSession?, _ success : Bool) -> Void) {
+        
+        let videoAsset: AVAsset = AVAsset( url: inputURL as URL )
+        let videoTrack = videoAsset.tracks( withMediaType: .video ).first! as AVAssetTrack
+        
+        let videoComposition = AVMutableVideoComposition()
+        videoComposition.renderSize = CGSize( width: fixedDimensions, height: fixedDimensions )
+        videoComposition.frameDuration = CMTimeMake(value: 1, timescale: Int32(fixedFramesPerSecond))
+        
+        
+        let instruction = AVMutableVideoCompositionInstruction()
+        instruction.timeRange = CMTimeRangeMake(start: .zero, duration: videoTrack.timeRange.duration)
+        
+        var scale : CGFloat = CGFloat(1.0)
+        if videoTrack.naturalSize.height > videoTrack.naturalSize.width {
+            scale = CGFloat(CGFloat(fixedDimensions)/videoTrack.naturalSize.width)
+        } else {
+            scale = CGFloat(CGFloat(fixedDimensions)/videoTrack.naturalSize.height)
+        }
+        
+        let transformer = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack)
+        transformer.setTransform(CGAffineTransform(scaleX: scale, y: scale), at: .zero)
+        
+        instruction.layerInstructions = [transformer]
+        videoComposition.instructions = [instruction]
+        
+        if let exportSession = AVAssetExportSession(asset: videoAsset, presetName: AVAssetExportPresetLowQuality) {
+            session(exportSession);
+            exportSession.outputURL = outputURL
+            exportSession.outputFileType = AVFileType.m4v
+            exportSession.videoComposition = videoComposition
+            exportSession.exportAsynchronously {
+                
+                if exportSession.status == .completed {
+                    print("Export complete")
+                    DispatchQueue.main.async(execute: {
+                        completion(exportSession,true)
+                    })
+                    return
+                } else if exportSession.status == .failed {
+                    print("Export error - \(String(describing: exportSession.error))")
+                    completion(exportSession,false)
+                }
+            }
+        } else {
+            print("Session could not be created")
+        }
+    }
+    
+    static func playVideo(videoURL : URL, presentingVC: UIViewController) {
+        let player = AVPlayer(url: videoURL)
+        let playerVC = AVPlayerViewController()
+        playerVC.player = player
+        presentingVC.present(playerVC, animated: true) {
+            playerVC.player!.play()
+        }
+    }
+}
--- VideoCompress/ViewController.swift
+++ VideoCompress/ViewController.swift
@@ -7,14 +7,91 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var progressBar : UIProgressView?
+    @IBOutlet private weak var pickVideoButton : UIButton?
+    @IBOutlet private weak var watchVideoButton : UIButton?
+    
+    private let videoPicker = UIImagePickerController()
+    private var progressTimer : Timer? = nil
+    private var exportedVideoURL : URL? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        videoPicker.delegate = self
     }
 
+    @IBAction private func pickVideo () {
+        videoPicker.allowsEditing = false
+        videoPicker.sourceType = .photoLibrary
+        videoPicker.mediaTypes = [kUTTypeMovie as String]
+        present(videoPicker, animated: true, completion: nil)
+    }
+    
+    @IBAction private func watchVideo() {
+        if let exportedURL = self.exportedVideoURL {
+            AVUtility.playVideo(videoURL: exportedURL, presentingVC: self)
+        }
+    }
+    
+    private func updateProgresssBar(progress : Float) {
+    
+        if let progressBar = self.progressBar {
+            progressBar.progress = progress
+            progressBar.isHidden = progress == 0.0 || progress == 1.0
+        }
+        
+    }
+}
 
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @objc func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let videoURL = info[UIImagePickerController.InfoKey.mediaURL] as? URL {
+            if videoURL.startAccessingSecurityScopedResource() {
+                if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum (videoURL.path)) {
+                    let outputURL = NSURL.fileURL(withPath: NSTemporaryDirectory() + NSUUID().uuidString + ".m4v")
+                    AVUtility.compressVideo(inputURL: videoURL, outputURL: outputURL, session: { (session) in
+                        self.pickVideoButton?.setTitle("Exporting", for: .normal)
+                        self.pickVideoButton?.isUserInteractionEnabled = false
+                        self.progressTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (timer) in
+                            if let validSession = session {
+                                self.updateProgresssBar(progress: validSession.progress)
+                            }
+                        })
+                        
+                    }) { (session, success) in
+                        self.pickVideoButton?.setTitle("Pick me!", for: .normal)
+                        self.pickVideoButton?.isUserInteractionEnabled = true
+                        self.progressTimer?.invalidate()
+                        self.progressTimer = nil
+                        if success {
+                            self.exportedVideoURL = outputURL
+                            if let button = self.watchVideoButton {
+                                if (button.isHidden) {
+                                    button.isHidden = false
+                                    button.alpha = 0.0
+                                    UIView.animate(withDuration: 0.3, animations: {
+                                        button.alpha = 1.0;
+                                    })
+                                    
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
